a35c0aeb810353a409be3c838d0dc074ccd8c303
Cassio Paes-Leme <cpaesleme@ngmoco.com>
2013-03-29 17:06:03 -0700

    added blank sheet for test 4

--- test/mobilize-hive_test.rb
+++ test/mobilize-hive_test.rb
@@ -25,6 +25,11 @@ describe "Mobilize" do
     hive_1_in_tsv = YAML.load_file("#{Mobilize::Base.root}/test/hive_test_1_in.yml").hash_array_to_tsv
     hive_1_in_sheet.write(hive_1_in_tsv,Mobilize::Gdrive.owner_name)
 
+    #create blank sheet
+    hive_4_in_sheet = Mobilize::Gsheet.find_by_path("#{r.path.split("/")[0..-2].join("/")}/hive_test_4.in",gdrive_slot)
+    [hive_4_in_sheet].each {|s| s.delete if s}
+    hive_4_in_sheet = Mobilize::Gsheet.find_or_create_by_path("#{r.path.split("/")[0..-2].join("/")}/hive_test_4.in",gdrive_slot)
+
     hive_1_schema_sheet = Mobilize::Gsheet.find_by_path("#{r.path.split("/")[0..-2].join("/")}/hive_test_1.schema",gdrive_slot)
     [hive_1_schema_sheet].each {|s| s.delete if s}
     hive_1_schema_sheet = Mobilize::Gsheet.find_or_create_by_path("#{r.path.split("/")[0..-2].join("/")}/hive_test_1.schema",gdrive_slot)
